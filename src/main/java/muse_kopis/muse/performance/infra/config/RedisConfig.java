51d157ccc6decc0725465d7cf2f63b9d5fef1dbd
potato <65760583+juntae6942@users.noreply.github.com>
2025-09-28 20:02:54 +0900

    feat: 실시간 연관 검색어 기능 구현

---
+++ src/main/java/muse_kopis/muse/performance/infra/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package muse_kopis.muse.performance.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
